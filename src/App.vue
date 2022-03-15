3eb7c442fc53d79fb0cea6a410a01e8c0b9024b9
Maarten van Laarhoven <32482953+reynard80@users.noreply.github.com>
2022-03-15 16:37:11 +0100

    Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,54 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+  <v-app
+      v-if="!$vuetify.breakpoint.mobile"
+      id="fitch"
+  >
+    <v-btn @click="showDialog=true">OPEN DIALOG</v-btn>
+    <v-dialog v-model="showDialog">
+      <v-card>
+        <v-toolbar color="red" dark flat>
+          <v-toolbar-title>
+            <v-icon class="mr-3">mdi-account</v-icon>
+            <span>TITLE</span>
+          </v-toolbar-title>
+          <template v-slot:extension>
+            <v-tabs v-model="activeTab" slider-size="3">
+              <v-tab v-for="(tab, index) in tabs" :key="index">
+                {{ tab.name }}
+              </v-tab>
+            </v-tabs>
+          </template>
+        </v-toolbar>
+        <v-tabs-items v-model="activeTab">
+          <v-tab-item v-for="(tab, index) in tabs" :key="index">
+            <component :is="tab.component" />
+          </v-tab-item>
+        </v-tabs-items>
+        <v-card-actions>
+          <v-btn @click="showDialog=false">CLOSE</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-app>
+  <div v-else>
+    Deze applicatie is niet geschikt voor mobiele devices.
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld';
+<script lang="ts">
+import Vue from 'vue';
 
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+export default Vue.extend({
+    name: 'App',
+    data() {
+        return {
+            showDialog: false,
+            activeTab: 0,
+            tabs: [
+                { name: 'tab 1' },
+                { name: 'tab 2' },
+            ],
+        };
+    },
+});
 </script>
--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,15 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'i18n/locales',
+      enableInSFC: false,
+      enableBridge: false
+    }
+  }
 })
